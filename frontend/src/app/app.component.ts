9a9ed8c7041f05efa785b1b409f69e01eaa9fdc9
Emmet Murray <e.minor.murray@gmail.com>
2018-09-14 23:40:13 +1000

    Better design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+  import { ListItemComponent } from './list-item/list-item.component';
 
 @Component({
   selector: 'app-root',
@@ -6,19 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello world!';
-  tasks = ['Write angular tutorial', 'panic', 'Nevermind, all g!'];
+  public title = 'My todo list!';
+  // A list of starter tasks
+  public tasks = ['Write angular tutorial','I\'s not compiling....','Nevermind, all g!'];
 
+  // Add an task to our task list
   addTask(newTask: string) {
     this.tasks.push(newTask);
   }
 
+  // Remove a tast from our list
   removeTask(task) {
-
-    console.log(task);
-    
+    // Get index of item
     var index = this.tasks.indexOf(task);
-    if (index == -1) console.log("f");
+    if (index === -1) return;
+    // Remove found task
     this.tasks.splice(index,1);
   }
 }
--- frontend/src/app/list-item/list-item.component.ts
+++ frontend/src/app/list-item/list-item.component.ts
@@ -7,11 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
+  // If we wanted to do a binding, we would use this
   @Input() task: string;
-  
-  constructor() { }
 
+  public completed: boolean;
+
+  // Doesn't do anything here
+  constructor() {}
+
+  // Will be run once, for initialisation if we choose not to use
+  // A constructor
   ngOnInit() {
+    this.completed = false;
   }
-
 }

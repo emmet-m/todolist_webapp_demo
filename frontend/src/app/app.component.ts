5b32d49b970ab2aaf59109ce814ff07bb92ae053
Emmet Murray <e.minor.murray@gmail.com>
2018-09-14 19:24:19 +1000

    Added Base - Waiting for backend connection!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-frontend';
+  title = 'Hello world!';
+  tasks = ['Write angular tutorial', 'panic', 'Nevermind, all g!'];
+
+  addTask(newTask: string) {
+    this.tasks.push(newTask);
+  }
+
+  removeTask(task) {
+
+    console.log(task);
+    
+    var index = this.tasks.indexOf(task);
+    if (index == -1) console.log("f");
+    this.tasks.splice(index,1);
+  }
 }
---
+++ frontend/src/app/list-item/list-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.css']
+})
+export class ListItemComponent implements OnInit {
+
+  @Input() task: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
